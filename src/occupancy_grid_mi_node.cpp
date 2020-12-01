41f6c1b165a29b93a8ad3d8fcbaea3e1cc00b1bb
bostoncleek <bocl8874@colorado.edu>
2020-11-30 18:23:28 -0600

    comment out debug print

--- src/occupancy_grid_mi_node.cpp
+++ src/occupancy_grid_mi_node.cpp
@@ -58,7 +58,7 @@ class OccupancyGridMI {
 
     void map_callback(const nav_msgs::OccupancyGrid & map_msg)
     {
-      ROS_INFO("Map received");
+      // ROS_INFO("Map received");
       map = map_msg;
     }
 
